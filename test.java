07036052b2eb72d28ffa86a2c9d1349e8eb51692
ayushs2725 <aysingh@ucsd.edu>
2022-04-29 15:22:27 -0700

    Added method and tester

--- test.java
+++ test.java
@@ -1,1 +1,7 @@
-
+public class test
+{
+    public static int multiply(int a, int b)
+    {
+        return a*b;
+    }
+}
---
+++ tester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class tester
+{
+    @Test
+    public void multiply()
+    {
+        assertEquals(9, test.multiply(3, 3));
+    }
+}
